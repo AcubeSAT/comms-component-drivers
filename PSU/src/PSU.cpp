8a1bfa137fe66f5270e635a3e463fada6643cad6
ntoylker <nikos.toulk@gmail.com>
2024-01-02 01:41:36 +0200

    oops mb

--- PSU/src/PSU.cpp
+++ PSU/src/PSU.cpp
@@ -24,8 +24,8 @@ namespace PSU {
 
     bool PG_read() {
         GPIO_PinState pinstate_temporary[3];
-        pinstate_temporary[0] = HAL_GPIO_ReadPin(P5V_RF_PG_GPIO_Port, P5V_FPGA_PG_Pin);
-        pinstate_temporary[1] = HAL_GPIO_ReadPin(P5V_RF_PG_GPIO_Port, P5V_FPGA_PG_Pin);
+        pinstate_temporary[0] = HAL_GPIO_ReadPin(P5V_FPGA_PG_GPIO_Port, P5V_FPGA_PG_Pin);
+        pinstate_temporary[1] = HAL_GPIO_ReadPin(P5V_RF_PG_GPIO_Port, P5V_RF_PG_Pin);
         pinstate_temporary[2] =  HAL_GPIO_ReadPin(P3V3_RF_PG_GPIO_Port, P3V3_RF_PG_Pin);
 
         if(pinstate_temporary[0] == GPIO_PIN_SET && pinstate_temporary[1] == GPIO_PIN_SET && pinstate_temporary[2] == GPIO_PIN_SET)
