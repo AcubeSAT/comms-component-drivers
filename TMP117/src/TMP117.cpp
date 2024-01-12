5a26ee54a81c37ec616999364909306df2a5e0b8
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2024-01-12 13:02:38 +0200

    potential fixes

--- TMP117/src/TMP117.cpp
+++ TMP117/src/TMP117.cpp
@@ -15,14 +15,14 @@ namespace TMP117 {
             return etl::make_pair(Error::Timeout, NULL);
         }
 
-        return etl::make_pair(Error::NoErrors, ((static_cast<uint16_t>(buf[0]) << 8) & 0xFF00 | buf[1]));
+        return etl::make_pair(Error::NoErrors, ((static_cast<uint16_t>(buf[0]) << 8) | buf[1]));
     }
 
     Error TMP117::writeRegister(RegisterAddress targetRegister, uint16_t value) {
         uint8_t buf[] = {static_cast<uint8_t>(targetRegister), static_cast<uint8_t>(value & 0xFF),
                          static_cast<uint8_t>((value >> 8) & 0xFF)};
 
-        if (HAL_I2C_Master_Transmit(&hi2c1, i2cSlaveAddress & 0x01, buf, 2, MaxTimeoutDelay) != HAL_OK) {
+        if (HAL_I2C_Master_Transmit(&hi2c1, i2cSlaveAddress, buf, 3, MaxTimeoutDelay) != HAL_OK) {
             return Error::Timeout;
         }
         return Error::NoErrors;
@@ -33,7 +33,7 @@ namespace TMP117 {
         if (eepromRegister > 3 || eepromRegister < 1) { return Error::InvalidEEPROM; }
         // Unlock EEPROM
         Error error;
-        error = TMP117::writeRegister(RegisterAddress::EEPROMUnlock, 0x4000);
+        error = TMP117::writeRegister(RegisterAddress::EEPROMUnlock, 0x8000);
         if (error != Error::NoErrors) { return error; }
 
         uint16_t timeoutCounter = MaxTimeoutDelay;
@@ -43,7 +43,7 @@ namespace TMP117 {
             timeoutCounter -= 7; // Rough estimation of elapsed ms
             auto[error, reg] = TMP117::readRegister(RegisterAddress::EEPROMUnlock);
             // Check if EEPROM is still busy
-            if (!(reg.value() && 0x8000)) {
+            if (!(reg.value() && 0x4000)) {
                 // If it's finished abort and start writing
                 break;
             }
@@ -96,7 +96,7 @@ namespace TMP117 {
             return etl::make_pair(error, NULL);
         }
 
-        return etl::make_pair(Error::NoErrors, reg.value() & 0xF >> 12);
+        return etl::make_pair(Error::NoErrors, (reg.value() >> 12) & 0xF);
     }
 
     etl::pair<Error, std::optional<float>> TMP117::setCalibrationOffset(float calibration) {
@@ -108,11 +108,11 @@ namespace TMP117 {
 
         if (calibration >= 0) {
             error = TMP117::writeRegister(RegisterAddress::TemperatureOffset,
-                                        static_cast<uint16_t>(calibration / temperaturePrecision));
+                                          static_cast<uint16_t>(calibration / temperaturePrecision));
             return etl::make_pair(error, NULL);
         } else {
             error = TMP117::writeRegister(RegisterAddress::TemperatureOffset,
-                                        ~(static_cast<uint8_t>(calibration / temperaturePrecision)) + 1);
+                                          ~(static_cast<uint8_t>(calibration / temperaturePrecision)) + 1);
             return etl::make_pair(error, NULL);
         }
     }
@@ -127,7 +127,7 @@ namespace TMP117 {
         if (reg.value() & 0x8000) {
             return etl::make_pair(NoErrors, (-(~reg.value() + 1)) * temperaturePrecision);
         } else {
-            return etl::make_pair(NoErrors, reg.value());
+            return etl::make_pair(NoErrors, reg.value() * temperaturePrecision);
         }
     }
 
@@ -149,17 +149,17 @@ namespace TMP117 {
         }
 
         if (!ignoreAlert) {
-            if (reg.value() && 0x8000) {
+            if (reg.value() & 0x8000) {
                 return etl::make_pair(Error::TemperatureHigh, NULL);
             }
 
-            if (reg.value() && 0x4000) {
+            if (reg.value() & 0x4000) {
                 return etl::make_pair(Error::TemperatureLow, NULL);
             }
         }
 
         // Continuous mode
-        if (!(reg.value() && 0xC00)) {
+        if ((reg.value()&0x0C00)>>10  == 0x00 || (reg.value()&0xC00)>>10  == 0x3) {
             auto[error, reg] = readRegister(RegisterAddress::TemperatureRegister);
             if (error != NoErrors) {
                 return etl::make_pair(error, NULL);
@@ -172,36 +172,37 @@ namespace TMP117 {
         }
 
         // One-shot mode
-        if ((reg.value() >> 10) & 0x3 == 0x2) {
-            error = TMP117::writeRegister(RegisterAddress::EEPROM1, reg.value() || 0xC00);
-        } // One-shot ongoing but data not ready yet
+        if (((reg.value() >> 10) & 0x3) == 0x2) {
+            // error = TMP117::writeRegister(RegisterAddress::EEPROM1, reg.value() || 0xC00);
+            // One-shot ongoing but data not ready yet
 
-        uint16_t timeout = MaxTimeoutDelay;
+            uint16_t timeout = MaxTimeoutDelay;
 
-        while (timeout > 0) {
-            HAL_Delay(TimeoutWait);
-            timeout -= TimeoutWait;
-            auto[error, reg] = readRegister(RegisterAddress::ConfigurationRegister);
-            if (error != NoErrors) {
-                return etl::make_pair(error, NULL);
-            }
-
-            if (reg.value() && 0x4) {
-                auto[error, reg] = readRegister(RegisterAddress::TemperatureRegister);
+            while (timeout > 0) {
+                HAL_Delay(TimeoutWait);
+                timeout -= TimeoutWait;
+                auto [error, reg] = readRegister(RegisterAddress::ConfigurationRegister);
                 if (error != NoErrors) {
                     return etl::make_pair(error, NULL);
                 }
 
-                return etl::make_pair(
-                        NoErrors,
-                        convertTemperature(reg.value()) + temperatureOffset
-                );
+                if (reg.value() & 0x2000) {
+                    auto [error, reg] = readRegister(RegisterAddress::TemperatureRegister);
+                    if (error != NoErrors) {
+                        return etl::make_pair(error, NULL);
+                    }
+
+                    return etl::make_pair(
+                            NoErrors,
+                            convertTemperature(reg.value()) + temperatureOffset
+                    );
+                }
             }
+            return etl::make_pair(
+                    Timeout,
+                    NULL
+            );
         }
-        return etl::make_pair(
-                Timeout,
-                NULL
-        );
     }
 
     etl::pair<Error, uint16_t> TMP117::getDeviceID() {
