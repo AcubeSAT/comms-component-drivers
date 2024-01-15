926a4597d5f45ec1408c0256e12eefd2ed845e9a
ntoylker <nikos.toulk@gmail.com>
2024-01-15 15:46:42 +0200

    add some comments about PSU_Class functions' behavior

--- PSU/inc/PSU.hpp
+++ PSU/inc/PSU.hpp
@@ -12,13 +12,25 @@ namespace PSU {
     public:
         PSU(void) {};
 
+        /* Sets P5V_FPGA_EN_Pin PD8 to state SET */
         static void enable_FPGA_PSU();
+        /* Resets P5V_FPGA_EN_Pin PD8 to state RESET */
         static void disable_FPGA_PSU();
+
+        /* Sets P5V_RF_EN pin PE13 to state SET */
         static void enable_RF_PSU();
+        /* Resets P5V_RF_EN pin PE13 to state RESET */
         static void disable_RF_PSU();
 
+        /* Reads the Power-Good (PG) signals from 3 pins:
+          P5V_FPGA_PG pin PD9
+          P5V_RF_PG pin PC7
+          P3V3_RF_PG Pin PA9
+        and returns 'true' if all 3 are in SET state or else 'false' */
         static bool PG_read();
 
+        /* Solves PG_fault error. If at least 1 pin out of the 3 returns RESET state,
+        this function checks which pin/pins has/have the problem and steps in to set in SET state*/
         static void solve_PG_fault();
     };
 }
