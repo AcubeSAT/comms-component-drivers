8fa046f8658cc6abe0cfd4a796e3f7a13382402f
ntoylker <nikos.toulk@gmail.com>
2024-01-17 13:55:32 +0200

    Almost finish PSU Class creation and add more detailed comments

--- PSU/inc/PSU.hpp
+++ PSU/inc/PSU.hpp
@@ -3,32 +3,53 @@
 #include <optional>
 #include <cstdint>
 #include "main.h"
-//#include "etl/utility.h"
 
-// creating a 'namespace' in order to not conflict variables' names with other coders
 namespace PSU {
 
     class PSU {
     public:
         PSU(void) {};
 
-        static bool isOff(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
+        /**
+         * @brief Checks whether the given Pin is OFF (RESET STATE)
+         * @param GPIOx: Where x can be (A..K) to select the GPIO peripheral.
+         * @param GPIO_Pin: specifies the port bit to be written.
+         *          This parameter can be one of GPIO_PIN_x where x can be (0..15).
+         * @return True if Pin is OFF, otherwise False
+         */
+        static bool isPinOff(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 
-        /* Sets given Pin to state SET */
+        /**
+         * @brief Sets given Pin to state SET
+         * @param GPIOx: Where x can be (A..K) to select the GPIO peripheral.
+         * @param GPIO_Pin: specifies the port bit to be written.
+         *          This parameter can be one of GPIO_PIN_x where x can be (0..15).
+         */
         static void enablePartPSU(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
-        /* Resets given Pin to state RESET */
+
+        /**
+         * @brief Resets given Pin to state RESET
+         * @param GPIOx: Where x can be (A..K) to select the GPIO peripheral.
+         * @param GPIO_Pin: specifies the port bit to be written.
+         *          This parameter can be one of GPIO_PIN_x where x can be (0..15).
+         */
         static void disablePartPSU(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
 
-        /* Reads the Power-Good (PG) signals from 3 pins:
-          P5V_FPGA_PG pin PD9
-          P5V_RF_PG pin PC7
-          P3V3_RF_PG Pin PA9
-        and returns 'true' if all 3 are in SET state or else 'false' */
-        static bool PG_read();
+        /**
+         * @brief Reads the Power-Good (PG) signals from 3 pins:
+         * P5V_FPGA_PG pin PD9,
+         * P5V_RF_PG pin PC7,
+         * P3V3_RF_PG Pin PA9,
+         * @return True if all 3 Pins are in SET state, otherwise False
+         */
+        static bool PGread();
 
-        /* */
+        /**
+         * @brief Solves the 'NO Power-Good Signal' error (PG signals went on RESET state) of the given PIN
+         * @param GPIOx: Where x can be (A..K) to select the GPIO peripheral.
+         * @param GPIO_Pin: specifies the port bit to be written.
+         *          This parameter can be one of GPIO_PIN_x where x can be (0..15).
+         */
         static void solvePGfault(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin);
     };
 }
-
-//#endif //STM32H7A3ZIQSETUP_PSU_HPP
--- PSU/src/PSU.cpp
+++ PSU/src/PSU.cpp
@@ -1,31 +1,25 @@
 #include "PSU.hpp"
 #include "stm32h7xx_hal_gpio.h"
 
-
-
 namespace PSU {
     void PSU::enablePartPSU(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
         HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_SET);
         HAL_Delay(100);
     }
-    void PSU::disablePartPSU(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
+
+    void PSU::disablePartPSU(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
         HAL_GPIO_WritePin(GPIOx, GPIO_Pin, GPIO_PIN_RESET);
         HAL_Delay(100);
     }
 
-    bool PSU::isOff(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
-        GPIO_PinState pinstate_temporary;
-        pinstate_temporary = HAL_GPIO_ReadPin(GPIOx, GPIO_Pin);
-
-        if (pinstate_temporary == GPIO_PIN_RESET)
-            return true;
-        else // is SET
-            return false;
+    bool PSU::isPinOff(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
+        return HAL_GPIO_ReadPin(GPIOx, GPIO_Pin) == GPIO_PIN_RESET;
     }
-    bool PSU::PG_read() {
-        if ( isOff(P5V_FPGA_PG_GPIO_Port, P5V_FPGA_PG_Pin)
-        || isOff(P5V_RF_PG_GPIO_Port, P5V_RF_PG_Pin)
-        || isOff(P3V3_RF_PG_GPIO_Port, P3V3_RF_PG_Pin) )
+
+    bool PSU::PGread() {
+        if ( isPinOff(P5V_FPGA_PG_GPIO_Port, P5V_FPGA_PG_Pin)
+        || isPinOff(P5V_RF_PG_GPIO_Port, P5V_RF_PG_Pin)
+        || isPinOff(P3V3_RF_PG_GPIO_Port, P3V3_RF_PG_Pin) )
             return false;
         else
             return true;
@@ -33,7 +27,7 @@ namespace PSU {
 
     void PSU::solvePGfault(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin) {
         // hops into a loop until the PIN is ON
-        while(isOff(GPIOx, GPIO_Pin)){
+        while(isPinOff(GPIOx, GPIO_Pin)){
             disablePartPSU(GPIOx, GPIO_Pin);
             enablePartPSU(GPIOx, GPIO_Pin);
         }
