53684df7212918e05ffd48151f274e590306eaa3
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2025-04-21 02:37:13 +0300

    Initial

---
+++ ExternalFrontend/inc/ExternalFrontend.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+#include <cstdint>
+#include "etl/optional.h"
+#include "stm32h7xx_hal_gpio.h"
+
+/// Flags that get set to true from a callback, in case the current limiters FLAGB pin goes
+///  LOW, due to output overcurrent, input undervoltage or overheating. The user may read them.
+inline volatile bool ExternalFrontend_currentLimiterAlertTxUHF = false;
+inline volatile bool ExternalFrontend_currentLimiterAlertRxUHF = false;
+inline volatile bool ExternalFrontend_currentLimiterAlertSBAND = false;
+
+namespace ExternalFrontend {
+    enum class ExternalFrontendChain {
+        TX_UHF,
+        RX_UHF,
+        TX_SBAND
+    };
+
+    /**
+     * A class that encapsulates all necessary functionality and state for the
+     * external frontend of the SatNOGS-COMMS board.
+     */
+    class ExternalFrontendUtilities {
+    public:
+         /**
+          * @note All chains will be set to inactive by default.
+          * @param hadcTemp Handler to ADC peripheral for AGC temperature conversion.
+          */
+         ExternalFrontendUtilities(ADC_HandleTypeDef* hadcTemp, ADC_HandleTypeDef* hadcOutVoltAgc,
+                                   DAC_HandleTypeDef* hdacSetpointVoltage):
+             txUhfActive(false), rxUhfActive(false), sbandActive(false),
+             hadcTemp(hadcTemp), hadcOutVoltAgc(hadcOutVoltAgc), hdacSetpointVoltage(hdacSetpointVoltage) {
+             HAL_GPIO_WritePin(EN_PA_UHF_GPIO_Port, EN_PA_UHF_Pin, GPIO_PIN_SET);
+             HAL_GPIO_WritePin(EN_UHF_AMP_RX__GPIO_Port, EN_UHF_AMP_RX__Pin, GPIO_PIN_RESET);
+             HAL_GPIO_WritePin(EN_RX_UHF_GPIO_Port, EN_RX_UHF_Pin, GPIO_PIN_SET);
+             HAL_GPIO_WritePin(EN_S_BAND_TX_GPIO_Port, EN_S_BAND_TX_Pin, GPIO_PIN_SET);
+         }
+
+        [[nodiscard]] bool getAgcSwitchedIn() const {
+            return agcSwitchedIn;
+        }
+
+        /**
+         *  @brief Open current limiters and enable components.
+         *  @param agcSwitchedIn Determines whether the AGC should be enabled or not (has affect only for RX_UHF chain).
+         *  @note There is a turn on time determined mainly by the current limiters. The user should begin
+         *        utilizing the frontend 16ms after calling this function.
+         *
+         *    TODO calculate and set here the setpoint voltage as a function of a more useful input parameter,
+         *         like the desired PA output power in dBm
+         */
+        void enableExternalFrontend(ExternalFrontendChain chain, bool agcSwitchedIn = false);
+
+        void disableExternalFrontend(ExternalFrontendChain chain);
+
+        /**
+         * @brief Read the temperature from the integrated analog sensor of the AGC peripheral.
+         *        The reference voltage is 600mV in 27 degrees Celsius, and the slope is 2mV/degree
+         * @returns The temperature in Celsius, if the Rx-UHF chain is active and the ADC conversion succeed.
+         */
+        [[nodiscard]] etl::optional<float> readAGCTemperature();
+
+        /**
+         * @brief Freeze AGC to its current gain
+         * @returns Whether operation was successful or not. False will also be returned in case the AGC is switched out.
+         *          In case of a successful operation, the ADC output is returned as well (for diagnostic purposes).
+         */
+        etl::pair<bool, uint16_t> freezeAGC();
+
+        /**
+         * @brief Return AGC to normal operation.
+         * @returns Whether operation was successful or not. False will also be returned in case the AGC is switched out.
+         */
+        [[nodiscard]] bool unfreezeAGC() const;
+
+    private:
+        /// State of external frontend chains
+        bool txUhfActive;
+        bool rxUhfActive;
+        bool sbandActive;
+
+        ///  Determines whether automatic gain control mode is used for the RX_UHF frontend, or a constant
+        ///  gain is applied instead.
+        bool agcSwitchedIn;
+        uint32_t setPointVoltage;
+
+        /// Handlers for ADC,DAC conversions (via polling mode)
+        ADC_HandleTypeDef* hadcTemp; // ADC handle for reading temperature pin of AGC
+        ADC_HandleTypeDef* hadcOutVoltAgc; // ADC handle for reading the AGC's current out voltage (which is used to set the AMP gain, if the AGC in switched in)
+        DAC_HandleTypeDef* hdacSetpointVoltage; // DAC handle for writing the setpoint voltage
+
+        /// Converted voltages are stored here
+        uint32_t voltageBufferTemp;
+        uint32_t voltageBufferOutVoltAGC;
+
+        /// Delays for waiting    // TODO find proper timings
+        static constexpr uint16_t tempConversionMaxDelayMs = 15;
+        static constexpr uint16_t outVoltAGCConversionMaxDelayMs = 15;
+
+        /// AGC Temperature linear characteristic variables
+        static constexpr float Slope = 2.0F;  // mv/degree
+        static constexpr float Vref = 600.0F; // mv
+        static constexpr float Tref = 27.0F;  // degrees
+
+        /**
+         * @brief Read the current output voltage of the AGC ()
+         * @note Used by freezeAGC()
+         * @returns Whether the operation was successful or not
+         */
+        bool readOutVoltageAGC();
+    };
+} // namespace ExternalFrontend
---
+++ ExternalFrontend/src/ExternalFrontend.cpp
@@ -0,0 +1,150 @@
+#include "main.h"
+#include "ExternalFrontend.hpp"
+
+#include <MCUTemperatureTask.hpp>
+
+#include "Task.hpp"
+namespace ExternalFrontend {
+    void ExternalFrontendUtilities::enableExternalFrontend(const ExternalFrontendChain chain, bool agcSwitchedIn) {
+        switch (chain) {
+            case ExternalFrontendChain::TX_UHF:
+                if (!txUhfActive) {
+                    // enable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_PA_UHF_GPIO_Port, EN_PA_UHF_Pin, GPIO_PIN_RESET);
+                    txUhfActive = true;
+                }
+                break;
+            case ExternalFrontendChain::RX_UHF:
+                if (!rxUhfActive) {
+                    // enable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_RX_UHF_GPIO_Port, EN_RX_UHF_Pin, GPIO_PIN_RESET);
+                    // enable the AMPLIFIER, LNA and the AGC (active high logic)
+                    HAL_GPIO_WritePin(EN_UHF_AMP_RX__GPIO_Port, EN_UHF_AMP_RX__Pin, GPIO_PIN_SET);
+                    rxUhfActive = true;
+
+                    // switch in the AGC (if requested)
+                    if (agcSwitchedIn) {
+                        HAL_GPIO_WritePin(EN_AGC_UHF_GPIO_Port, EN_AGC_UHF_Pin, GPIO_PIN_RESET);
+                    } else {
+                        HAL_GPIO_WritePin(EN_AGC_UHF_GPIO_Port, EN_AGC_UHF_Pin, GPIO_PIN_SET);
+                    }
+                }
+                break;
+            case ExternalFrontendChain::TX_SBAND:
+                if (!sbandActive) {
+                    // enable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_S_BAND_TX_GPIO_Port, EN_S_BAND_TX_Pin, GPIO_PIN_RESET);
+                    sbandActive = true;
+                }
+                break;
+        }
+    }
+
+    void ExternalFrontendUtilities::disableExternalFrontend(ExternalFrontendChain chain) {
+        switch (chain) {
+            case ExternalFrontendChain::TX_UHF:
+                if (txUhfActive) {
+                    // disable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_PA_UHF_GPIO_Port, EN_PA_UHF_Pin, GPIO_PIN_SET);
+                    txUhfActive = false;
+                }
+                break;
+            case ExternalFrontendChain::RX_UHF:
+                if (rxUhfActive) {
+                    // disable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_RX_UHF_GPIO_Port, EN_RX_UHF_Pin, GPIO_PIN_SET);
+                    // disable the AMPLIFIER, LNA and the AGC (active high logic)
+                    HAL_GPIO_WritePin(EN_UHF_AMP_RX__GPIO_Port, EN_UHF_AMP_RX__Pin, GPIO_PIN_RESET);
+                    rxUhfActive = false;
+                }
+                break;
+            case ExternalFrontendChain::TX_SBAND:
+                if (sbandActive) {
+                    // disable current limiter (active low logic)
+                    HAL_GPIO_WritePin(EN_S_BAND_TX_GPIO_Port, EN_S_BAND_TX_Pin, GPIO_PIN_SET);
+                    sbandActive = false;
+                }
+                break;
+        }
+    }
+
+    etl::optional<float> ExternalFrontendUtilities::readAGCTemperature() {
+        if (!rxUhfActive) {
+            return {};
+        }
+
+        // Calibrate peripheral for better accuracy
+        if (HAL_ADCEx_Calibration_Start(hadcTemp, ADC_CALIB_OFFSET, ADC_SINGLE_ENDED) != HAL_OK) {
+            return {};
+        }
+
+        if (HAL_ADC_PollForConversion(hadcTemp, tempConversionMaxDelayMs) != HAL_OK) {
+            return {};
+        }
+
+        voltageBufferTemp = HAL_ADC_GetValue(hadcTemp);
+        HAL_ADC_Stop(hadcTemp);
+
+        // TODO confirm the 3.3 voltage
+        const float voltageFloat = static_cast<float>(voltageBufferTemp) * (3.3F/ 65536.0F);  // assuming 3.3V power supply and 16 bit resolution
+        return (1000.0F * voltageFloat - Vref) / Slope + Tref;
+    }
+
+    bool ExternalFrontendUtilities:: readOutVoltageAGC() {
+        // Calibrate peripheral for better accuracy
+        if (HAL_ADCEx_Calibration_Start(hadcOutVoltAgc, ADC_CALIB_OFFSET, ADC_SINGLE_ENDED) != HAL_OK) {
+            return {};
+        }
+
+        if (HAL_ADC_PollForConversion(hadcOutVoltAgc, outVoltAGCConversionMaxDelayMs) != HAL_OK) {
+            return {};
+        }
+
+        voltageBufferOutVoltAGC = HAL_ADC_GetValue(hadcOutVoltAgc);
+        HAL_ADC_Stop(hadcOutVoltAgc);
+        return true;
+    }
+
+    etl::pair<bool, uint16_t> ExternalFrontendUtilities::freezeAGC() {
+        if (!rxUhfActive || !agcSwitchedIn) {
+            return etl::make_pair(false, 0);
+        }
+
+        // read current output value by agc
+        if (!readOutVoltageAGC()) {
+            return etl::make_pair(false, 0);
+        }
+
+        // use it as the new setpoint voltage
+        if (HAL_DAC_Start(hdacSetpointVoltage, DAC_CHANNEL_2) != HAL_OK) {
+            return etl::make_pair(false, static_cast<uint16_t>(voltageBufferOutVoltAGC));
+        }
+
+        if (HAL_DAC_SetValue(hdacSetpointVoltage, DAC_CHANNEL_2, DAC_ALIGN_12B_R, voltageBufferOutVoltAGC) != HAL_OK) {
+            return etl::make_pair(false, static_cast<uint16_t>(voltageBufferOutVoltAGC));
+        }
+
+        // switch out the AGC
+        HAL_GPIO_WritePin(EN_AGC_UHF_GPIO_Port, EN_AGC_UHF_Pin, GPIO_PIN_SET);
+        return etl::make_pair(true, static_cast<uint16_t>(voltageBufferOutVoltAGC));
+    }
+
+    bool ExternalFrontendUtilities::unfreezeAGC() const {
+        if (!rxUhfActive || !agcSwitchedIn) {
+            return false;
+        }
+
+        // use the defined setpoint voltage
+        if (HAL_DAC_Start(hdacSetpointVoltage, DAC_CHANNEL_2) != HAL_OK) {
+            return false;
+        }
+
+        if (HAL_DAC_SetValue(hdacSetpointVoltage, DAC_CHANNEL_2, DAC_ALIGN_12B_R, setPointVoltage) != HAL_OK) {
+            return false;
+        }
+
+        // switch in the AGC
+        HAL_GPIO_WritePin(EN_AGC_UHF_GPIO_Port, EN_AGC_UHF_Pin, GPIO_PIN_RESET);
+        return true;
+    }
+} // namespace ExternalFrontend
