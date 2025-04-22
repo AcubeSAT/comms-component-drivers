8ea2f090e7d550f60ba0f3b7ce83864c90dbbba0
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2025-04-22 12:14:24 +0300

    Organize driver to use a class based interface, for consistency with other drivers.

--- eMMC/inc/eMMC.hpp
+++ eMMC/inc/eMMC.hpp
@@ -1,87 +1,14 @@
 #pragma once
 #include "stm32h7xx_hal.h"
 #include "etl/expected.h"
-#include <array>
 #include <cstdint>
 #include "FreeRTOS.h"
 #include <semphr.h>
 
-
-struct memoryQueueItemHandler {
-    uint32_t size;
-    uint32_t startPage;
-};
-
-#define MEMORY_ITEM(name, size)
-#define MEMORY_QUEUE(queue_name, item_size, queue_size)                                                                                        \
-    static inline uint8_t queue_name##QueueStorageArea[sizeof(memoryQueueItemHandler) * queue_size] __attribute__((section(".dtcmram_data"))); \
-    inline QueueHandle_t queue_name##Queue;                                                                                                    \
-    static inline StaticQueue_t queue_name##QueueBuffer;
-
-#include "MemoryItems.def"
-#undef MEMORY_ITEM
-#undef MEMORY_QUEUE
-
 namespace eMMC {
-
-    struct eMMCTransactionHandler {
-        SemaphoreHandle_t eMMC_semaphore;
-        bool WriteComplete = false;
-        bool ReadComplete = false;
-        bool ErrorOccured = false;
-        bool transactionAborted = false;
-        MMC_HandleTypeDef hmmcSnapshot;
-        uint32_t transactionTimeoutPerBlock = 100; // ms
-        uint32_t getSemaphoreTimeout = 1000;       //ms
-    };
-    extern eMMCTransactionHandler eMMCTransactionHandler;
-
-    // Declare constants
-    extern uint64_t memoryCapacity;
-    extern const uint32_t memoryPageSize; // bytes
-
-    // Define the enum using the MEMORY_ITEM macro from the definition file
-#define MEMORY_ITEM(name, size) name,
-#define MEMORY_QUEUE(name, item_size, queue_size)
-    enum memoryItem {
-#include "MemoryItems.def"
-        memoryItemCount // This is automatically added after all items
-    };
-#undef MEMORY_ITEM
-#undef MEMORY_QUEUE
-
-#define MEMORY_ITEM(name, size)
-#define MEMORY_QUEUE(queue_name, item_size, queue_size) queue_name,
-    enum memoryQueue {
-#include "MemoryItems.def"
-        memoryQueueCount // This is automatically added after all items
-    };
-#undef MEMORY_ITEM
-#undef MEMORY_QUEUE
-
-    struct memoryQueueHandler {
-        QueueHandle_t* queue;
-        uint32_t sizeOfItem;
-        uint32_t numberOfItems;
-        uint32_t tailPageOffset;
-        uint32_t firstPage;
-        uint32_t lastPage;
-        uint64_t startAddress;
-        memoryQueueHandler() : sizeOfItem(0), queue(NULL), tailPageOffset(0), numberOfItems(0), startAddress(0), firstPage(0), lastPage(0) {}
-        explicit memoryQueueHandler(uint32_t newSize, uint32_t newNumberOfItems, QueueHandle_t* newQueue)
-                : sizeOfItem(newSize), numberOfItems(newNumberOfItems), queue(newQueue), tailPageOffset(0), startAddress(0), firstPage(0), lastPage(0) {}
-    };
-    struct memoryItemHandler {
-        // memoryItemData();
-        // explicit memoryItemData(uint32_t newSize);
-        uint32_t size;
-        uint32_t startAddress;
-        uint32_t endAddress;
-        memoryItemHandler() : size(0), startAddress(0), endAddress(0) {}
-        explicit memoryItemHandler(uint32_t newSize)
-                : size(newSize), startAddress(0), endAddress(0) {}
-    };
-    // Error status
+    /**
+     * Error status
+     */
     enum class Error : uint8_t {
         NO_ERRORS = 0,
         EMMC_READ_FAILURE,
@@ -90,99 +17,137 @@ namespace eMMC {
         EMMC_INVALID_NUM_OF_BLOCKS,
         EMMC_INVALID_START_ADDRESS_ON_ERASE,
         EMMC_TRANSACTION_TIMED_OUT,
-        EMMC_BUFFER_TOO_SMALL
+        EMMC_BUFFER_TOO_SMALL,
+        EMMC_QUEUE_FULL,
+        EMMC_QUEUE_EMPTY
     };
 
-    extern std::array<memoryItemHandler, memoryItemCount> memoryMap;
-    extern std::array<memoryQueueHandler, memoryQueueCount> memoryQueueMap;
-
-    void eMMCMemoryInit();
-
     /**
-     * 
-     * @param write_data 
-     * @param block_address 
-     * @param numberOfBlocks
-     * @return 
+     * Used by ISR to indicate an eMMC transaction is complete.
      */
-    etl::expected<void, Error> writeBlockEMMC(uint8_t* write_data, uint32_t block_address, uint32_t numberOfBlocks);
+    struct EMMCTransactionFlags {
+        bool WriteComplete = false;
+        bool ReadComplete = false;
+        bool ErrorOccured = false;
+        bool transactionAborted = false;
+    };
+    extern EMMCTransactionFlags eMMCTransactionFlags;
 
     /**
-     * 
-     * @param read_data 
-     * @param block_address 
-     * @param numberOfBlocks
-     * @return 
+     * @details Memory item: A generic data structure, which can be useful when the user needs to store
+     *              only one item or needs to manage the underlying memory blocks manually.
+     * @note Define your memory items in MemoryItems.def
      */
-    etl::expected<void, Error> readBlockEMMC(uint8_t* read_data, uint32_t block_address, uint32_t numberOfBlocks);
+#define MEMORY_ITEM(name, size) name,
+    enum MemoryItem {
+#include "MemoryItems.def"
+        memoryItemCount // This is automatically added after all items
+    };
+#undef MEMORY_ITEM
 
     /**
-      *
-      * @param itemHandler
-      * @param dataBuffer
-      * @param bufferSize
-      * @param startBlock
-      * @param numOfBlocks
-      * @return
-      */
-    etl::expected<void, Error> getItem(memoryItemHandler itemHandler, uint8_t* dataBuffer, uint32_t bufferSize, uint32_t startBlock, uint32_t numOfBlocks);
-
-    /**
-      *
-      * @param queueHandler
-      * @param itemHandler
-      * @param buffer
-      * @param bufferSize
-      * @return
-      */
-    etl::expected<void, Error> getItemFromQueue(memoryQueueHandler queueHandler, memoryQueueItemHandler itemHandler, uint8_t* buffer, uint32_t bufferSize);
-
-    /**
-      *
-      * @param queueHandler
-      * @param itemHandler
-      * @param buffer
-      * @param bufferSize
-      * @return
-      */
-    etl::expected<void, Error> storeItemInQueue(memoryQueueHandler queueHandler, memoryQueueItemHandler* itemHandler, uint8_t* buffer, uint32_t bufferSize);
-
-    /**
-    *
-    * @param itemHandler
-    * @param dataBuffer
-    * @param bufferSize
-    * @return
-    */
-    etl::expected<void, Error> getItem(memoryItemHandler itemHandler, uint8_t* dataBuffer, uint32_t bufferSize);
-
-
-    /**
-     *
-     * @param itemHandler
-     * @param dataBuffer
-     * @param bufferSize
-     * @param startBlock
-     * @param numOfBlocks
-     * @return
+     * @details Memory queue: A data structure with push and pop operations.
+     * @note Define your queues in MemoryQueues.def
      */
-    etl::expected<void, Error> storeItem(memoryItemHandler itemHandler, uint8_t* dataBuffer, uint32_t bufferSize, uint32_t startBlock, uint32_t numOfBlocks);
+#define MEMORY_QUEUE(queue_name, item_size, queue_size) queue_name,
+    enum MemoryQueue {
+#include "MemoryQueues.def"
+        memoryQueueCount // This is automatically added after all items
+    };
+#undef MEMORY_QUEUE
 
-    /**
-     *
-     * @param itemHandler
-     * @param dataBuffer
-     * @param bufferSize
-     * @return
-     */
-    etl::expected<void, Error> storeItem(memoryItemHandler itemHandler, uint8_t* dataBuffer, uint32_t bufferSize);
+    class eMMC_Utilities {
+    public:
+        explicit eMMC_Utilities();
 
-    /**
-     * 
-     * @param block_address_start 
-     * @param block_address_end 
-     * @return 
-     */
-    etl::expected<void, Error> eraseBlocksEMMC(uint32_t block_address_start, uint32_t block_address_end);
+        void registerMMCHandle(MMC_HandleTypeDef* handle) {
+            hmmc = handle;
+        }
 
+        /** Memory item interface**/
+        etl::expected<void, Error> getItem(MemoryItem item, uint8_t* destBuffer, uint32_t bufferSize);
+
+        etl::expected<void, Error> getItem(MemoryItem item, uint8_t* destBuffer, uint32_t bufferSize, uint32_t startBlock, uint32_t numOfBlocks);
+
+        etl::expected<void, Error> storeItem(MemoryItem item, uint8_t* sourceBuffer, uint32_t bufferSize);
+
+        etl::expected<void, Error> storeItem(MemoryItem item, uint8_t* sourceBuffer, uint32_t bufferSize, uint32_t startBlock, uint32_t numOfBlocks);
+
+        /** Queue interface **/
+        etl::expected<void, Error> popItemFromQueue(MemoryQueue queue, uint8_t* destBuffer, uint32_t bufferSize);
+
+        etl::expected<void, Error> pushItemToQueue(MemoryQueue queue, uint8_t* sourceBuffer, uint32_t bufferSize);
+
+    private:
+        /**
+         * Size parameters for the SDINBDG4-8G
+         */
+        static constexpr uint32_t memoryPageSize = 512;
+        static constexpr uint32_t memoryPageCount = 0xE90E80 * memoryPageSize;
+        static constexpr uint32_t memorySizeInBytes = memoryPageSize * memoryPageCount;
+
+        /**
+         * Transaction handling parameters
+         */
+        MMC_HandleTypeDef *hmmc;
+        SemaphoreHandle_t eMMC_semaphoreHandle;
+        StaticSemaphore_t eMMC_semaphoreBuffer;
+        uint32_t transactionTimeoutPerBlock = 100; // ms
+        uint32_t getSemaphoreTimeout = 1000;       //ms
+
+        /**
+         * Hold state for memory regions that store a single item
+         */
+        struct MemoryItemHandler {
+            uint32_t size;
+            uint32_t startAddress;
+            uint32_t endAddress;
+            MemoryItemHandler() : size(0), startAddress(0), endAddress(0) {}
+            explicit MemoryItemHandler(const uint32_t newSize)
+                    : size(newSize), startAddress(0), endAddress(0) {}
+        };
+
+        etl::array<MemoryItemHandler, memoryItemCount> memoryItemMap;
+
+        /**
+         * Hold state for memory regions that store a queue of items
+         */
+        struct MemoryQueueHandler {
+            QueueHandle_t* queue;
+            uint32_t sizeOfItem;
+            uint32_t numberOfItems;
+            uint32_t tailPageOffset;
+            uint32_t firstPage;
+            uint32_t lastPage;
+            uint64_t startAddress;
+            MemoryQueueHandler() : queue(NULL), sizeOfItem(0), numberOfItems(0), tailPageOffset(0), firstPage(0), lastPage(0), startAddress(0) {}
+            explicit MemoryQueueHandler(const uint32_t newSize, const uint32_t newNumberOfItems, QueueHandle_t* newQueue)
+                    : queue(newQueue), sizeOfItem(newSize), numberOfItems(newNumberOfItems), tailPageOffset(0), firstPage(0), lastPage(0), startAddress(0) {}
+        };
+
+#define MEMORY_QUEUE(queue_name, item_size, queue_size)                                                                                                \
+        inline static uint8_t queue_name##QueueStorageArea[sizeof(MemoryQueueHandler) * queue_size] __attribute__((section(".dtcmram_data")));         \
+        QueueHandle_t queue_name##Queue;                                                                                                               \
+        inline static StaticQueue_t queue_name##QueueBuffer;
+#include "MemoryQueues.def"
+#undef MEMORY_QUEUE
+
+        etl::array<MemoryQueueHandler, memoryQueueCount> memoryQueueMap;
+
+        /**
+         * @brief Write to eMMC blocks
+         */
+        etl::expected<void, Error> writeBlockEMMC(const uint8_t* sourceBuffer, uint32_t block_address, uint32_t numberOfBlocks);
+
+        /**
+         * @brief Read from eMMC blocks
+         */
+        etl::expected<void, Error> readBlockEMMC(uint8_t* destBuffer, uint32_t block_address, uint32_t numberOfBlocks) const;
+
+        /**
+         * @brief Erases specified memory region from eMMC
+         */
+        etl::expected<void, Error> eraseBlocksEMMC(uint32_t block_address_start, uint32_t block_address_end);
+    };
+    extern eMMC_Utilities eMMC_Utils;
 } // namespace eMMC
