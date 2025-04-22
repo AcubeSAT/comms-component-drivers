394925d980d267efe88eb8dda08c40480da7067c
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2025-04-22 12:44:39 +0300

    fix small error

--- AT86RF215/src/at86rf215.cpp
+++ AT86RF215/src/at86rf215.cpp
@@ -1241,7 +1241,7 @@ namespace AT86RF215 {
     }
 
     etl::expected<void, Error> At86rf215_Utilities::check_transceiver_connection(Error& err) {
-        DevicePartNumber dpn = transceiver.get_part_number(err);
+        const DevicePartNumber dpn = get_part_number(err);
         if (err == Error::NO_ERRORS && dpn == DevicePartNumber::AT86RF215) {
             return {}; /// success
         } else {
