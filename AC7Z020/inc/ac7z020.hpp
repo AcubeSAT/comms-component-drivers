7d743c520ba6d01b0fcc06dde73acc7012026003
giorgos_chatziathanasiou <giorgoschatziathasiou@gmail.com>
2025-05-01 19:25:04 +0300

    - Add semaphore for concurrent access protection - Add functions for reading fpga registers - DMA based reading and writing

--- AC7Z020/inc/ac7z020.hpp
+++ AC7Z020/inc/ac7z020.hpp
@@ -1,43 +1,94 @@
 #pragma once
 
+#include <utility>
 #include "ac7z020definitions.hpp"
 #include "stm32h7xx_hal.h"
-#include <utility>
-
-const uint16_t AC7Z020_TIMEOUT = 1000;
+#include "etl/expected.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "Logger.hpp"
 
 namespace AC7Z020 {
 
-    enum Error {
-        NO_ERRORS,
+    enum class Error : uint8_t {
         FAILED_WRITING_TO_FPGA,
-        INVALID_TM_LENGTH,
-        FAILED_READING_FROM_REGISTER
+        FAILED_READING_FROM_FPGA,
+        RESOURCE_MUTEX_TIMEOUT,
+        INVALID_IRQ_CODE,
+        INVALID_LENGTH
     };
 
-    class AC7Z020 {
+    class AC7Z020_Utilities {
     public:
-        /*
-         * Initializer for AC7Z020
-         *
-         * @param hspi: pointer to the SPI_HandleTypeDef responsible for configuring the SPI.
-         *
+
+        /// Binary semaphores for signaling certain events (add them inside the proper ISR or freertos task)
+        SemaphoreHandle_t spiWriteCompleteSemaphoreHandle;          // completion of spi write from dma callback
+        SemaphoreHandle_t spiReadCompleteSemaphoreHandle;           // completion of spi read from dma callback
+
+        /**
+         * Initializer for AC7Z020 driver
          */
-        AC7Z020(SPI_HandleTypeDef *hspim) :
-                hspi(hspim) {
-        };
+        AC7Z020_Utilities() {
+            resourcesMutexHandle = xSemaphoreCreateMutexStatic(&resourcesMutexBuffer);
+            spiWriteCompleteSemaphoreHandle = xSemaphoreCreateBinaryStatic(&spiWriteCompleteSemaphoreBuffer);
+            spiReadCompleteSemaphoreHandle = xSemaphoreCreateBinaryStatic(&spiReadCompleteSemaphoreBuffer);
+            if (resourcesMutexHandle == nullptr ||
+                spiWriteCompleteSemaphoreHandle == nullptr ||
+                spiReadCompleteSemaphoreHandle == nullptr) {
+                LOG_ERROR << "[AC7Z020 Driver] Failed to create semaphores";
+            }
+        }
 
-        void set_packet_length(PacketLength length, Error &err);
+        /**
+         * @warning Do not use the driver before calling this function.
+         */
+        void registerSPIHandle(SPI_HandleTypeDef* handle) {
+            hspi = handle;
+        }
 
-        void spi_block_write_8(uint16_t n, uint8_t *value,
-                               Error &err);
+        /**
+         * Read irq register
+         *
+         * @returns The irq code.
+         */
+        etl::expected<IrqCode, Error> readIrqCode();
 
-        void spi_block_read_8(uint16_t n, uint8_t *value,
-                               Error &err);
+        /**
+         * Transmit SBAND frame to FPGA
+         * @param sourceBuff The buffer containing the frame to transmit. It's length must be LEN_SBAND_FRAME bytes long
+         */
+        etl::expected<void, Error> transmitTMFrameSBAND(uint8_t* sourceBuff);
+
+        /**
+         * Transmit UHF frame to FPGA
+         * @param sourceBuff The buffer containing the frame to transmit. It's length must be LEN_UHF_TM_FRAME bytes long
+         */
+        etl::expected<void, Error> transmitTMFrameUHF(uint8_t* sourceBuff);
+
+        /**
+         * Receive a UHF frame from the FPGA
+         * @param destBuff A buffer to place the received frame. It's length must be LEN_UHF_TC_FRAME_LEN
+         *                 (the maximum expected UHF TC frame length)
+         *
+         *  @returns The length of the received frame
+         */
+        etl::expected<uint16_t, Error> receiveTCFrameUHF(uint8_t* destBuff);
 
     private:
         SPI_HandleTypeDef *hspi;
-        uint16_t packet_length;
+
+        /// Mutex for concurrent access protection
+        StaticSemaphore_t resourcesMutexBuffer = {};
+        SemaphoreHandle_t resourcesMutexHandle;
+        uint16_t resouceMutexTimeout = 100; // in ms
+
+        /// Binary semaphores for signaling external events
+        StaticSemaphore_t spiWriteCompleteSemaphoreBuffer = {};
+        StaticSemaphore_t spiReadCompleteSemaphoreBuffer = {};
+
+        etl::expected<void, Error> spi_block_write_8(uint16_t regAddress, uint8_t* sourceBuff, uint16_t numBytes);
+        etl::expected<void, Error> spi_block_read_8(uint16_t regAddress, uint8_t* destBuff, uint16_t numBytes);
     };
 
+    extern AC7Z020_Utilities ac7z020Utils;
 }
--- AC7Z020/src/ac7z020.cpp
+++ AC7Z020/src/ac7z020.cpp
@@ -3,44 +3,124 @@
 
 namespace AC7Z020 {
 
-    void AC7Z020::spi_block_write_8(uint16_t n, uint8_t *value, Error &err) {
+    etl::expected<void, Error> AC7Z020_Utilities::spi_block_write_8(uint16_t regAddress, uint8_t* sourceBuff, uint16_t numBytes) {
+        uint8_t msg[2] = {static_cast<uint8_t>(0x80 | ((regAddress >> 8) & 0x7F)), static_cast<uint8_t>(regAddress & 0xFF)};
+        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_RESET); // slave select pin
 
-        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_RESET);
-        uint8_t hal_error = HAL_SPI_Transmit(hspi, value, n, AC7Z020_TIMEOUT);
-
-        if (hal_error != HAL_OK) {
-            err = Error::FAILED_WRITING_TO_FPGA;
-            return;
+        uint8_t hal_error = HAL_SPI_Transmit_DMA(hspi, msg, 2);
+        if (hal_error != HAL_OK ||
+            xSemaphoreTake(spiWriteCompleteSemaphoreHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
+            return etl::unexpected(Error::FAILED_WRITING_TO_FPGA);
         }
-        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
 
-        err = Error::NO_ERRORS;
-    }
-
-    void AC7Z020::spi_block_read_8(uint16_t n, uint8_t *value, Error &err) {
-
-        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_RESET);
-        uint8_t hal_error = HAL_SPI_Receive(hspi, value, n, AC7Z020_TIMEOUT);
-
-        if (hal_error != HAL_OK) {
-            err = Error::FAILED_READING_FROM_REGISTER;
-            return;
+        hal_error = HAL_SPI_Transmit_DMA(hspi, sourceBuff, numBytes);
+        if (hal_error != HAL_OK ||
+            xSemaphoreTake(spiWriteCompleteSemaphoreHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
+            return etl::unexpected(Error::FAILED_WRITING_TO_FPGA);
         }
 
         HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
-        err = Error::NO_ERRORS;
-
-        return;
+        return {};
     }
 
-    void AC7Z020::set_packet_length(PacketLength length, Error &err) {
-        if (length != UHFTMPacketLength
-            && length != SBandPacketLength) {
-            err = Error::INVALID_TM_LENGTH;
+    etl::expected<void, Error> AC7Z020_Utilities::spi_block_read_8(uint16_t regAddress, uint8_t* destBuff, uint16_t numBytes) {
+        uint8_t msg[2] = {static_cast<uint8_t>((regAddress >> 8) & 0x7F), static_cast<uint8_t>(regAddress & 0xFF)};
+
+        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_RESET); // slave select pin
+        uint8_t hal_error = HAL_SPI_TransmitReceive_DMA(hspi, msg, destBuff, numBytes + 2);
+
+        if (hal_error != HAL_OK ||
+            xSemaphoreTake(spiReadCompleteSemaphoreHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
+            return etl::unexpected(Error::FAILED_READING_FROM_FPGA);
         }
-        packet_length = length;
+
+        HAL_GPIO_WritePin(FPGA_NSS_GPIO_Port, FPGA_NSS_Pin, GPIO_PIN_SET);
+        return {};
     }
 
+    etl::expected<IrqCode, Error> AC7Z020_Utilities::readIrqCode() {
+        if (xSemaphoreTake(resourcesMutexHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            return etl::unexpected(Error::RESOURCE_MUTEX_TIMEOUT);
+        }
 
+        uint8_t irq_reg_val;
+        auto status = spi_block_read_8(RegisterAddress::IRQ, &irq_reg_val, 1);
+        xSemaphoreGive(resourcesMutexHandle);
+
+        auto irq = static_cast<IrqCode>(irq_reg_val);
+        if (irq != IrqCode::UHF_PREAMBLE_RECEIVED &&
+            irq != IrqCode::UHF_FULL_FRAME_RECEIVED &&
+            irq != IrqCode::UHF_FULL_FRAME_TRANSMISSION_COMPLETE &&
+            irq != IrqCode::SBAND_FULL_FRAME_TRANSMISSION_COMPLETE) {
+            return etl::unexpected(Error::INVALID_IRQ_CODE);
+        }
+
+        return irq;
+    }
+
+    etl::expected<void, Error> AC7Z020_Utilities::transmitTMFrameSBAND(uint8_t* sourceBuff) {
+        if (xSemaphoreTake(resourcesMutexHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            return etl::unexpected(Error::RESOURCE_MUTEX_TIMEOUT);
+        }
+
+        auto status =
+            spi_block_write_8(RegisterAddress::SBAND_FRAME, sourceBuff, FrameRegisterLength::LEN_SBAND_FRAME);
+        xSemaphoreGive(resourcesMutexHandle);
+
+        if (!status.has_value()) {
+            return etl::unexpected(status.error());
+        }
+
+        return {};
+    }
+
+    etl::expected<void, Error> AC7Z020_Utilities::transmitTMFrameUHF(uint8_t* sourceBuff) {
+        if (xSemaphoreTake(resourcesMutexHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            return etl::unexpected(Error::RESOURCE_MUTEX_TIMEOUT);
+        }
+
+        auto status =
+            spi_block_write_8(RegisterAddress::UHF_TM_FRAME, sourceBuff,
+                FrameRegisterLength::LEN_UHF_TM_FRAME);
+        xSemaphoreGive(resourcesMutexHandle);
+
+        if (!status.has_value()) {
+            return etl::unexpected(status.error());
+        }
+
+        return {};
+    }
+
+    etl::expected<uint16_t, Error> AC7Z020_Utilities::receiveTCFrameUHF(uint8_t* destBuff) {
+        if (xSemaphoreTake(resourcesMutexHandle, pdMS_TO_TICKS(resouceMutexTimeout)) != pdTRUE) {
+            return etl::unexpected(Error::RESOURCE_MUTEX_TIMEOUT);
+        }
+
+        // read the frame's length
+        uint8_t length_reg_val[2];
+        auto status = spi_block_read_8(static_cast<uint16_t>(RegisterAddress::UHF_TC_FRAME_LEN),
+            length_reg_val, 2);
+
+        uint16_t length = (static_cast<uint16_t>(length_reg_val[0]) << 8) | length_reg_val[1];
+        if (length == 0 || length > FrameRegisterLength::LEN_UHF_TC_FRAME) {
+            xSemaphoreGive(resourcesMutexHandle);
+            return etl::unexpected(Error::INVALID_LENGTH);
+        }
+
+        // read the frame
+        status = spi_block_read_8(RegisterAddress::UHF_TC_FRAME_LEN, destBuff, length);
+        xSemaphoreGive(resourcesMutexHandle);
+
+        if (!status.has_value()) {
+            return etl::unexpected(status.error());
+        }
+
+        return length;
+    }
+
+    AC7Z020_Utilities ac7z020Utils = AC7Z020_Utilities();
 }
 
